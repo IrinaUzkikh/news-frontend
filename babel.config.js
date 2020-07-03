d3e33d060fd109f643f76cd0ca4fdee4bc89f272
Irina Uzkikh <uzkikh-i@yandex.ru>
2020-07-03 17:59:24 +0300

    корректировки, до адаптива

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,35 @@
-const presets = [
+/* const presets = [
   [
     '@babel/env',
     {
-      targets: { // указать цели, для полифилов
-        edge: '15',
+      targets: {
+        edge: '17',
         ie: '11',
         firefox: '50',
         chrome: '64',
         safari: '11.1',
+        ChromeAndroid: '42'
       },
-      useBuiltIns: 'usage', // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
-      corejs: '3.4.1', // явно проставить версию corejs
+      useBuiltIns: 'usage',
+      corejs: '3.4.1',
+    },
+  ],
+]; */
+
+const presets = [
+  [
+    '@babel/env',
+    {
+      targets: [
+        'last 2 Chrome versions',
+        'last 2 ChromeAndroid versions',
+        'last 2 Firefox versions',
+        'last 2 Safari versions',
+        'Edge >= 15',
+      ],
+      useBuiltIns: 'usage',
+      corejs: '3.4.1',
     },
   ],
 ];
-
 module.exports = { presets };
